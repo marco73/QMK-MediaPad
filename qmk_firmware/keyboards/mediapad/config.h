72c11539cbf1ee88f23851658a27d3aa5ff13f4a
marco73 <wauwwie8@hotmail.com>
2021-03-08 22:42:16 +0100

    update

--- qmk_firmware/keyboards/mediapad/config.h
+++ qmk_firmware/keyboards/mediapad/config.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x2021
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    marco73
+#define PRODUCT         2x4 keypad with an encoder
+
+#define BACKLIGHT_PIN   B0
