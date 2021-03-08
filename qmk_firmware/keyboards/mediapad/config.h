79a67f504c5842dfe1fb4472fd04277a2039ab40
marco73 <wauwwie8@hotmail.com>
2021-03-08 15:41:10 +0000

    Update config.h
    Added keymaps

---
+++ qmk_firmware/keyboards/mediapad/config.h
@@ -0,0 +1,21 @@
+{
+  "keyboard_name": "Mediapad",
+  "url": "",
+  "maintainer": "marco73",
+  "width": 4,
+  "height": 2,
+  "layouts": {
+    "LAYOUT_ortho_4x2": {
+      "layout": [
+        {"label":"1", "x":0, "y":0},
+        {"label":"2", "x":1, "y":0},
+        {"label":"3", "x":2, "y":0},
+        {"label":"4", "x":3, "y":0},
+        {"label":"5", "x":0, "y":1},
+        {"label":"6", "x":1, "y":1},
+        {"label":"7", "x":2, "y":1},
+        {"label":"8", "x":3, "y":1}
+      ]
+    }
+  }
+}
